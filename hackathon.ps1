d614a4109f10488e56d2530c50912e9741e6bd11
Jackson McGuire <jaxmcguire@gmail.com>
2022-06-24 13:14:35 -0400

    Add updates and suggestions from Bob

--- hackathon.ps1
+++ hackathon.ps1
@@ -1,9 +1,10 @@
-﻿
+﻿# This script performs installations of Microsoft Edge, NotePad++, and Google Chrome, as well as disabling various security and network protocols.
+
+# Function that takes in an Action or an AppName, and with that sends a message to the Host explaining that the task has been carried out succesfuly. 
 function Write-ActionLog {
     param (
         [string]$AppName,
-        [string]$Action,
-        [string]$Copied
+        [string]$Action
     )
     $date = Get-Date
 
@@ -11,38 +12,54 @@ function Write-ActionLog {
     if (-Not ($AppName -eq ''))
     {
         Write-Host "$AppName has been succesfully Installed at $date."
-    } elseif (-Not ($Copied -eq '')) {
-        Write-Host "$Copied has been succesfully copied to the logging directory at $date."
     } else {
         Write-Host "$Action has been succesfully disabled at $date."
     }
 }
-
+# Function that takes in a Protocol Parameter, and with that Protocol, will go into the Systems Security providers protocols and Disable that Protocol at the Client and Server Level if the files are already present, otherwise it will create those files with Disabled Values.
 function Disable-Protocols {
     param(
         [Parameter(Mandatory = $true)]
         [string]$Protocol
     )
-    New-Item "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\PROTOCOLS\$Protocol\Server" -Force
-    New-Item "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\PROTOCOLS\$Protocol\Client" -Force
-    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\PROTOCOLS\$Protocol\Server" -name 'Enabled' -value '0' -PropertyType 'DWORD'
-    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\PROTOCOLS\$Protocol\Server" -Name 'DisabledByDefault' -value '1' -PropertyType 'DWORD'
-    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\PROTOCOLS\$Protocol\Client" -name 'Enabled' -value '0' -PropertyType 'DWORD'
-    New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\PROTOCOLS\$Protocol\Client" -Name 'DisabledByDefault' -value '1' -PropertyType 'DWORD'
+    $Server = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\PROTOCOLS\$Protocol\Server"
+    $Client = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\PROTOCOLS\$Protocol\Client"
+    $ServerExists = Test-Path -Path $Server
+    $ClientExists = Test-Path -Path $Client
+    if (!(($ServerExists -eq $True) -and ($ClientExists -eq $True))) {
+        New-Item $Server -Force
+        New-Item $Client -Force
+        New-ItemProperty -Path $Server -name 'Enabled' -value '0' -PropertyType 'DWORD'
+        New-ItemProperty -Path $Client -Name 'DisabledByDefault' -value '1' -PropertyType 'DWORD'
+        New-ItemProperty -Path $Server -name 'Enabled' -value '0' -PropertyType 'DWORD'
+        New-ItemProperty -Path $Client -Name 'DisabledByDefault' -value '1' -PropertyType 'DWORD'
+    } elseif (!($ServerExists -eq $True)) {
+        New-Item $Server -Force
+        New-ItemProperty -Path $Server -name 'Enabled' -value '0' -PropertyType 'DWORD'
+        New-ItemProperty -Path $Server -name 'Enabled' -value '0' -PropertyType 'DWORD'
+    } elseif (!($ClientExists -eq $True)) {
+        New-Item $Client -Force
+        New-ItemProperty -Path $Client -Name 'DisabledByDefault' -value '1' -PropertyType 'DWORD'
+        New-ItemProperty -Path $Client -Name 'DisabledByDefault' -value '1' -PropertyType 'DWORD'
+    } else {
+        Set-ItemProperty -Path $Server -name 'Enabled' -value '0'
+        Set-ItemProperty -Path $Client -Name 'DisabledByDefault' -value '1'
+        Set-ItemProperty -Path $Server -name 'Enabled' -value '0'
+        Set-ItemProperty -Path $Client -Name 'DisabledByDefault' -value '1'
+    }
 
     Write-ActionLog -Action $Protocol
 }
-
+# Function to Invoke the Disable-Protocols function for all Protocols
 function Disable-TLS-SSL {
     Disable-Protocols -Protocol "TLS 1.0"
     Disable-Protocols -Protocol "TLS 1.1"
     Disable-Protocols -Protocol "SSL 3.0"
 }
 
-
-mkdir -path .\logs
+mkdir -path C:\Windows\Logs\ProgramData
 mkdir -path .\installs
-
+# Function that takes in an App Name, optional Installer and optional Link parameters, and goes through and downloads the installer from the link given, executes that installer and saves the installation logs to. 
 function Install-Apps {
     param(
     [string]$Installer,
@@ -54,18 +71,18 @@ function Install-Apps {
     )
 
     if ($App -eq "Edge") {
-        Start-Process -FilePath ".\Browsers\MicrosoftEdgeSetup.exe" -RedirectStandardOutput ".\logs\$App.log" -PassThru -Wait
+        Start-Process -FilePath ".\Browsers\MicrosoftEdgeSetup.exe" -RedirectStandardOutput "C:\Windows\Logs\ProgramData\$App.log" -PassThru -Wait
     } elseif ($App -eq "Chrome") {
         Invoke-WebRequest $Link -OutFile ".\installs\$Installer.exe"
-        Start-Process -FilePath ".\installs\$Installer.exe" -RedirectStandardOutput ".\logs\$app.log" -PassThru -Wait
+        Start-Process -FilePath ".\installs\$Installer.exe" -RedirectStandardOutput "C:\Windows\Logs\ProgramData\$app.log" -PassThru -Wait
     } else {
         Invoke-WebRequest $Link -OutFile ".\installs\$Installer.exe"
-        Start-Process -FilePath ".\installs\$Installer.exe" -Argument "/silent" -RedirectStandardOutput ".\logs\$app.log" -PassThru -Wait
+        Start-Process -FilePath ".\installs\$Installer.exe" -Argument "/silent" -RedirectStandardOutput "C:\Windows\Logs\ProgramData\$app.log" -PassThru -Wait
     }
     
     Write-ActionLog -AppName $App
 }
-
+# Grabs All of the network adapter configurations, and iterates through them, disabling NetBios on each. Also Disables all IPv6 protocols.
 function Disable-NetBios-IPv6 {
 
     $AdapterConfigs = Get-WmiObject win32_networkadapterconfiguration
@@ -97,3 +114,4 @@ Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -Remove
 Write-ActionLog -Action "SMB1"
 
 Disable-NetBios-IPv6
+Remove-Item .\installs
